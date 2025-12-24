7c011fa2f39ee445c1b46dd5c82513281a487a48
Oeungrotha12 <oeungrotha12@gmail.com>
2025-12-24 10:07:10 +0700

    update

--- notification.js
+++ notification.js
@@ -1,23 +1,32 @@
+// ការតំឡើង និង Import Library
+//read bot_token & chat_id from .envfile // create web server
+// create path for calling file // call api from telegram bot
+// catch port for run on port 5000
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
+// កំណត់ Port និង Server
 const PORT = 5000;
 const app = express();
 
+// អាន env Variable
+// CHAT_ID → Telegram chat/group/channel ដែលអ្នកចង់ផ្ញើ BOT_TOKEN → Token របស់ Telegram Bot
 const CHAT_ID = process.env.CHAT_ID;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
+// for req and res
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// POST endpoint for product form
+// ទទួល data ពី form when submit
 app.post("/submit", async (req, res) => {
   try {
     const { productname, price, qty, description, created_date, CategoryName } = req.body;
 
-    // validation
+    // validation 
+    // catch error if no data
     if (!productname || !price || !qty) {
       return res.status(400).send("Product name, price, and quantity are required.");
     }
@@ -29,8 +38,8 @@ app.post("/submit", async (req, res) => {
         .replaceAll("<", "&lt;")
         .replaceAll(">", "&gt;");
 
-    // message to telegram
-    const text = `<b>New Product Entry</b>
+    // format message to telegram
+    const text = `<b>New Product 🆕</b>
 <b>Product:</b> ${escape(productname)}
 <b>Price:</b> ${escape(price)}
 <b>Quantity:</b> ${escape(qty)}
@@ -38,6 +47,7 @@ app.post("/submit", async (req, res) => {
 <b>Created Date:</b> ${escape(created_date || new Date().toLocaleDateString())}
 <b>Category:</b> ${escape(CategoryName || "N/A")}`;
 
+// Call Telegram API
     const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 
     const resp = await axios.post(url, null, {
@@ -48,14 +58,15 @@ app.post("/submit", async (req, res) => {
       },
     });
 
+    // catch tele success & error
     if (resp.data.ok) {
-  // Instead of redirect, send JSON
-  return res.json({ success: true });
-} else {
-  console.error("Telegram error:", resp.data);
-  return res.status(500).json({ success: false, error: "Failed to send notification." });
-}
-
+      // Instead of redirect, send JSON
+      return res.json({ success: true });
+    } else {
+      console.error("Telegram error:", resp.data);
+      return res.status(500).json({ success: false, error: "Failed to send notification." });
+    }
+// err.message or err.response.data
   } catch (err) {
     console.error(err?.response?.data || err.message);
     res.status(500).send("Server error");
